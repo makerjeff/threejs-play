33993e45f8037d0fe76dcbf86ccf7896b28374bb
Jefferson Wu <jefferson.wu@180la.com>
2016-04-19 19:09:21 -0700

    basic studies of shapes and lines

---
+++ README.md
@@ -0,0 +1,19 @@
+#180 Faces Hardware
+
+Based off of Node Serial experiments <br>
+
+###Technologies
+- Express
+- Jade **(new)** (for socket.io namespace exploration)
+- Colors
+- [Socket.io](http://socket.io/)
+- [Three.JS](https://github.com/mrdoob/three.js/wiki/Getting-Started)
+
+
+
+###Tutorials Base
+1. [Three.JS: Getting Started](https://github.com/mrdoob/three.js/wiki/Getting-Started)
+2. [Three.JS: Drawing Lines] (https://github.com/mrdoob/three.js/wiki/Drawing-lines)
+
+
+###NOTES
---
+++ ns-play-app.js
@@ -0,0 +1,104 @@
+/**
+ * 180 Faces - Hardware Interface
+ * Created by jefferson.wu on 4/11/16.
+ * Namespaced + Jade
+ *
+ * TODO: create a new random object with local RGBholder values on 'connect', Destroy on 'disconnect'
+ * TODO: environmental variables.  Use them.
+ */
+
+// SOCKET.IO BOILERPLATE
+var express = require('express');
+var http = require('http');
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io')(server);
+// SOCKET.IO BOILERPLATE - END
+
+var jade = require('jade');
+var colors = require('colors');
+
+// MIDDLEWARE
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'jade');
+
+//enable CORS
+app.use(function(request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+// GLOBALS
+var allClients = [];
+var port = process.argv[2];
+
+// APP info
+var appInfo = {
+    app: 'ThreeJS Experiments',
+    version: '0.0.3',
+    author: {
+        name: 'Jefferson Wu',
+        email: 'jefferson.wu@180la.com'
+    }
+};
+
+
+// ROUTES
+app.get('/', function(request, response){
+    response.render('index', appInfo);
+});
+
+
+/*SOCKET.IO NAMESPACE*/
+var namespaceString = 'love33';
+var nsp = io.of('/' + namespaceString);
+
+nsp.on('connection', function(socket){
+    console.log(socket.client.id.toString().blue + ' has connected to namespace: ' + namespaceString);
+});
+
+/*SOCKET.IO*/
+io.on('connection', function(socket){
+    allClients.push(socket);
+    console.log(socket.client.id.toString().blue + ' connected');
+
+    numberOfClients(allClients);
+
+    //greeting message, fired at the beginning
+    socket.emit('message', 'welcome message!');
+
+    // ==== SOCKET EVENTS ====
+
+    //on receiving of data
+    socket.on('data', function(data){
+    });
+
+    //on disconnect
+    socket.on('disconnect', function(){
+    });
+
+    // ==== SOCKET EVENTS - END ====
+});
+
+initServer(port);
+
+// ==== HELPER FUNCTIONS ====
+//TODO: move these to /helpers
+
+function initServer(port){
+    var serverPort = port || 3000;  //if no port, default to 3000
+    server.listen(serverPort);
+    console.log('Starting server on port ' + serverPort.rainbow);
+}
+
+/**
+ * Return and log to console the number of connected clients.
+ * @param allClientsArr The 'allClients' array
+ */
+function numberOfClients(allClientsArr){
+    console.log('Total clients: ' + allClientsArr.length);
+    return allClientsArr.length;
+}
+
